2f1214d29ed541c8c2b7ae0cfe40e887ead2a2ff
WYing333 <e0634980@u.nus.edu>
2024-03-02 18:07:46 -0500

    change the landpage

--- my-dapp/src/components/Transfer.tsx
+++ my-dapp/src/components/Transfer.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { TezosToolkit } from "@taquito/taquito";
+import { transferTemporaryOwnership } from "../contract.tsx";
 
 const Transfer = ({
   Tezos,
@@ -7,17 +8,30 @@ const Transfer = ({
   Tezos: TezosToolkit;
 }): JSX.Element => {
   const [recipient, setRecipient] = useState<string>("");
-  const [amount, setAmount] = useState<string>("");
+  const [token_id, setTokenID] = useState<string>("");
+  const [percentage, setPercentage] = useState<string>("");
+  const [duration, setDuration] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
 
   const sendTez = async (): Promise<void> => {
-    if (recipient && amount) {
+    if (recipient && token_id && percentage && duration) {
       setLoading(true);
-      try {
-        const op = await Tezos.wallet
-          .transfer({ to: recipient, amount: parseInt(amount), mutez: true })
-          .send();
-        await op.confirmation();
+      try { 
+
+        const opHash = await transferTemporaryOwnership(
+          'contract_address', // Replace with your contract address
+          parseInt(token_id),               
+          recipient,
+          parseInt(duration),           
+        );
+        //console.log(opHash)
+
+        // If transferTemporaryOwnership is successful, proceed to transfer Tezos
+        // const op = await Tezos.wallet
+        //   // .transfer({ to: recipient, parameter: {amount: parseInt(amount), percentage: parseInt(percentage), duratioin:}, mutez: true })
+        //   .transfer({ to: recipient, amount: parseInt(token_id), mutez: true })
+        //   .send();
+        // await op.confirmation();
       } catch (error) {
         console.log(error);
       } finally {
@@ -27,24 +41,53 @@ const Transfer = ({
   };
 
   return (
-    <div id="transfer-inputs">
-      Recipient: <input
+    <div>
+    <header style={{ backgroundColor: '#ffffff', padding: '25px' }}>
+      <div className="is-size-5"><strong>Demo</strong></div>
+    </header>
+
+    <div id="transfer-inputs" style={{ padding: '20px' }}>
+      Recipient:   {' '} <input
         type="text"
-        placeholder="Recipient"
+        placeholder="Recipient's Address"
         value={recipient}
         onChange={e => setRecipient(e.target.value)}
       />
       <br />
-      Amount in uTez:<input
+      <div style={{ height: '15px' }}></div>
+      Token ID:   {' '} <input
         type="number"
-        placeholder="Amount"
-        value={amount}
-        onChange={e => setAmount(e.target.value)}
+        placeholder="Token ID"
+        value={token_id}
+        onChange={e => setTokenID(e.target.value)}
       />
       <br />
-      <button
+      <div style={{ height: '15px' }}></div>
+      Royalty Percentage:   {' '}
+        <select
+          value={percentage}
+          onChange={e => setPercentage(e.target.value)}
+        >
+          <option value="5">5%</option>
+          <option value="10">10%</option>
+          <option value="15">15%</option>
+          <option value="20">20%</option>
+          <option value="25">25%</option>
+          <option value="30">30%</option>
+        </select>
+      <br />
+      <div style={{ height: '15px' }}></div>
+      Duration:   {' '} <input
+        type="number"
+        placeholder="Duration (Days)"
+        value={duration}
+        onChange={e => setDuration(e.target.value)}
+      />
+      <br />
+      <div style={{ height: '30px' }}></div>
+      <button id="send-button"
         className="button"
-        disabled={!recipient && !amount}
+        disabled={!recipient && !token_id && !percentage}
         onClick={sendTez}
       >
         {loading ? (
@@ -53,11 +96,13 @@ const Transfer = ({
           </span>
         ) : (
           <span>
-            <i className="far fa-paper-plane"></i>&nbsp; Send
+            <i className="far fa-paper-plane"></i>&nbsp;Send
           </span>
         )}
       </button>
     </div>
+
+  </div>
   );
 };
 
